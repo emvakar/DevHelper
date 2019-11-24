382e17105ac69b3ba5f95b9dceb5d7241f9e8b95
Emil Karimov <emvakar@gmail.com>
2019-11-24 03:39:49 +0300

    segment features

--- Sources/DevHelper/Extensions/DHSegment+Extension.swift
+++ Sources/DevHelper/Extensions/DHSegment+Extension.swift
@@ -16,32 +16,35 @@ public extension UISegmentedControl {
 
         self.setTitleTextAttributes([
             NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17 * UIScreen.main.bounds.width / 375, weight: .medium),
-            NSAttributedString.Key.foregroundColor: unselectedColor
+            NSAttributedString.Key.foregroundColor: unselectedColor,
+            NSAttributedString.Key.underlineColor: unselectedColor,
+            NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
             ], for: .normal)
 
         self.setTitleTextAttributes([
             NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17 * UIScreen.main.bounds.width / 375, weight: .medium),
-            NSAttributedString.Key.foregroundColor: selectedColor
+            NSAttributedString.Key.foregroundColor: selectedColor,
+            NSAttributedString.Key.underlineColor: selectedColor,
+            NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue
             ], for: .selected)
-
     }
 
     public func setupSegment(selectedColor: UIColor, unselectedColor: UIColor) {
         self.removeBorder(selectedColor: selectedColor, unselectedColor: unselectedColor)
-        let segmentUnderlineHeight: CGFloat = 1.0
-        let segmentUnderline = UIView()
-        segmentUnderline.backgroundColor = unselectedColor
-
-        self.addSubview(segmentUnderline)
-
-        segmentUnderline.snp.remakeConstraints {
-            $0.left.equalToSuperview()
-            $0.right.equalToSuperview()
-            $0.height.equalTo(segmentUnderlineHeight)
-            $0.bottom.equalToSuperview().offset(-segmentUnderlineHeight)
-        }
-
-        self.addUnderlineForSelectedSegment(selectedColor: selectedColor)
+//        let segmentUnderlineHeight: CGFloat = 1.0
+//        let segmentUnderline = UIView()
+//        segmentUnderline.backgroundColor = unselectedColor
+//
+//        self.addSubview(segmentUnderline)
+//
+//        segmentUnderline.snp.remakeConstraints {
+//            $0.left.equalToSuperview()
+//            $0.right.equalToSuperview()
+//            $0.height.equalTo(segmentUnderlineHeight)
+//            $0.bottom.equalToSuperview().offset(-segmentUnderlineHeight)
+//        }
+//
+//        self.addUnderlineForSelectedSegment(selectedColor: selectedColor)
 
     }
 
@@ -63,18 +66,18 @@ public extension UISegmentedControl {
     }
 
     public func changeUnderlinePosition() {
-        guard let underline = self.viewWithTag(997) else { return }
-        let underlineFinalXPosition = (self.frame.width / CGFloat(self.numberOfSegments)) * CGFloat(selectedSegmentIndex)
-        underline.snp.remakeConstraints {
-            if underlineFinalXPosition == 0 {
-                $0.left.equalToSuperview()
-                $0.right.equalTo(self.snp.centerX)
-            } else {
-                $0.left.equalTo(self.snp.centerX)
-                $0.right.equalToSuperview()
-            }
-            $0.height.equalTo(1)
-            $0.bottom.equalToSuperview().offset(-1)
-        }
+//        guard let underline = self.viewWithTag(997) else { return }
+//        let underlineFinalXPosition = (self.frame.width / CGFloat(self.numberOfSegments)) * CGFloat(selectedSegmentIndex)
+//        underline.snp.remakeConstraints {
+//            if underlineFinalXPosition == 0 {
+//                $0.left.equalToSuperview()
+//                $0.right.equalTo(self.snp.centerX)
+//            } else {
+//                $0.left.equalTo(self.snp.centerX)
+//                $0.right.equalToSuperview()
+//            }
+//            $0.height.equalTo(1)
+//            $0.bottom.equalToSuperview().offset(-1)
+//        }
     }
 }
