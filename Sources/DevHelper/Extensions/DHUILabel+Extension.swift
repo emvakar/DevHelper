b6793c6fee548556d9d94e6d0de6bd134fa75354
Emil Karimov <emvakar@gmail.com>
2020-02-26 14:35:04 +0300

    new method for integrate image into label

--- Sources/DevHelper/Extensions/DHUILabel+Extension.swift
+++ Sources/DevHelper/Extensions/DHUILabel+Extension.swift
@@ -8,11 +8,57 @@
 import UIKit
 
 public extension UILabel {
-    
+
     public static func makeLabel(size: CGFloat = 13, weight: UIFont.Weight = .regular, color: UIColor = UIColor.black) -> UILabel {
         let label = UILabel()
         label.font = UIFont.systemFont(ofSize: size, weight: weight)
         label.textColor = color
         return label
+    }
+
+
+    public func set(text: String, leftIcon: UIImage? = nil, rightIcon: UIImage? = nil, heigh: CGFloat? = nil, width: CGFloat? = nil) {
+
+        let leftAttachment = NSTextAttachment()
+        leftAttachment.image = leftIcon
+        leftAttachment.bounds = CGRect(x: 0, y: -2.5, width: 20, height: 20)
+        
+        if let width = width, let height = heigh {
+            leftAttachment.bounds = CGRect(x: 0, y: -2.5, width: width, height: height)
+        } else if let leftIcon = leftIcon {
+            leftAttachment.bounds = CGRect(x: 0, y: -2.5, width: leftIcon.size.width, height: leftIcon.size.height)
+        }
+        let leftAttachmentStr = NSAttributedString(attachment: leftAttachment)
+
+        let myString = NSMutableAttributedString(string: "")
+
+        let rightAttachment = NSTextAttachment()
+        rightAttachment.image = rightIcon
+        rightAttachment.bounds = CGRect(x: 0, y: -5, width: 20, height: 20)
+        let rightAttachmentStr = NSAttributedString(attachment: rightAttachment)
+
+
+        if semanticContentAttribute == .forceRightToLeft {
+            if rightIcon != nil {
+                myString.append(rightAttachmentStr)
+                myString.append(NSAttributedString(string: " "))
+            }
+            myString.append(NSAttributedString(string: text))
+            if leftIcon != nil {
+                myString.append(NSAttributedString(string: " "))
+                myString.append(leftAttachmentStr)
+            }
+        } else {
+            if leftIcon != nil {
+                myString.append(leftAttachmentStr)
+                myString.append(NSAttributedString(string: " "))
+            }
+            myString.append(NSAttributedString(string: text))
+            if rightIcon != nil {
+                myString.append(NSAttributedString(string: " "))
+                myString.append(rightAttachmentStr)
+            }
+        }
+        attributedText = myString
     }
 }
